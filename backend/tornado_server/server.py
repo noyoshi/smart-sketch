93e1c46c336f9a1f48d46251e154a5780e5c1202
Gabe Magee <gabe@Gabes-MacBook-Pro.local>
2019-04-12 23:01:17 -0700

    image uploader

--- backend/tornado_server/server.py
+++ backend/tornado_server/server.py
@@ -27,6 +27,18 @@ class MainHandler(BaseHandler):
         # Other methods: self.redirect, self.get_argument, self.request.body,
 
 
+class UploadHandler(tornado.web.RequestHandler):
+    def post(self):
+        file1 = self.request.files['file1'][0]
+        original_fname = file1['filename']
+        extension = os.path.splitext(original_fname)[1]
+        fname = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(6))
+        final_filename= fname+extension
+        output_file = open("uploads/" + final_filename, 'w')
+        output_file.write(file1['body'])
+        self.finish("file" + final_filename + " is uploaded")
+
+
 class MainApplication(tornado.web.Application):
     def __init__(self, **settings):
         tornado.web.Application.__init__(self, **settings)
