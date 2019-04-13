5b5bb793274acd56cf1c082a2ebc0156cad260ac
Tina Wu <tinawu.nd23@gmail.com>
2019-04-13 14:50:04 -0700

    drawing pad done

--- drawing/index.js
+++ drawing/index.js
@@ -37,13 +37,13 @@ document.getElementById('saveToImage').addEventListener('click', function () {
 }, false);
 document.getElementById('eraser').addEventListener('click', eraser);
 document.getElementById('clear').addEventListener('click', createCanvas);
-document.getElementById('save').addEventListener('click', save);
-document.getElementById('load').addEventListener('click', load);
-document.getElementById('clearCache').addEventListener('click', function () {
-  localStorage.removeItem("savedCanvas");
-  linesArray = [];
-  console.log("Cache cleared!");
-});
+// document.getElementById('save').addEventListener('click', save);
+// document.getElementById('load').addEventListener('click', load);
+// document.getElementById('clearCache').addEventListener('click', function () {
+//   localStorage.removeItem("savedCanvas");
+//   linesArray = [];
+//   console.log("Cache cleared!");
+// });
 
 // REDRAW 
 
@@ -89,33 +89,33 @@ function downloadCanvas(link, canvas, filename) {
 
 // SAVE FUNCTION
 
-function save() {
-  localStorage.removeItem("savedCanvas");
-  localStorage.setItem("savedCanvas", JSON.stringify(linesArray));
-  console.log("Saved canvas!");
-}
+// function save() {
+//   localStorage.removeItem("savedCanvas");
+//   localStorage.setItem("savedCanvas", JSON.stringify(linesArray));
+//   console.log("Saved canvas!");
+// }
 
 // LOAD FUNCTION
 
-function load() {
-  if (localStorage.getItem("savedCanvas") != null) {
-    linesArray = JSON.parse(localStorage.savedCanvas);
-    var lines = JSON.parse(localStorage.getItem("savedCanvas"));
-    for (var i = 1; i < lines.length; i++) {
-      ctx.beginPath();
-      ctx.moveTo(linesArray[i - 1].x, linesArray[i - 1].y);
-      ctx.lineWidth = linesArray[i].size;
-      ctx.lineCap = "round";
-      ctx.strokeStyle = linesArray[i].color;
-      ctx.lineTo(linesArray[i].x, linesArray[i].y);
-      ctx.stroke();
-    }
-    console.log("Canvas loaded.");
-  }
-  else {
-    console.log("No canvas in memory!");
-  }
-}
+// function load() {
+//   if (localStorage.getItem("savedCanvas") != null) {
+//     linesArray = JSON.parse(localStorage.savedCanvas);
+//     var lines = JSON.parse(localStorage.getItem("savedCanvas"));
+//     for (var i = 1; i < lines.length; i++) {
+//       ctx.beginPath();
+//       ctx.moveTo(linesArray[i - 1].x, linesArray[i - 1].y);
+//       ctx.lineWidth = linesArray[i].size;
+//       ctx.lineCap = "round";
+//       ctx.strokeStyle = linesArray[i].color;
+//       ctx.lineTo(linesArray[i].x, linesArray[i].y);
+//       ctx.stroke();
+//     }
+//     console.log("Canvas loaded.");
+//   }
+//   else {
+//     console.log("No canvas in memory!");
+//   }
+// }
 
 // ERASER HANDLING
 
