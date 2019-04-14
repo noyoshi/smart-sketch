1079e4ece13cc6bde896aa4c1fa19017a62982c4
Tina Wu <tinawu.nd23@gmail.com>
2019-04-13 23:00:02 -0700

    color selector

--- drawing/index.js
+++ drawing/index.js
@@ -19,15 +19,177 @@ document.getElementById('canvasUpdate').addEventListener('click', function () {
   createCanvas();
   redraw();
 });
-document.getElementById('colorpicker').addEventListener('change', function () {
-  currentColor = this.value;
-});
-document.getElementById('bgcolorpicker').addEventListener('change', function () {
-  ctx.fillStyle = this.value;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  redraw();
-  currentBg = ctx.fillStyle;
-});
+
+jQuery(document).ready(function ($) {
+
+  $color_list = [
+
+    { color: '#384f83', title: 'sea' },
+
+    { color: '#efefef', title: 'cloud' },
+
+    { color: '#2c1e16', title: 'dirt' },
+
+    { color: '#5d6e32', title: 'bush' },
+
+    { color: '#b7d24e', title: 'grass' },
+
+    { color: '#3c3b4b', title: 'mountain' },
+
+    { color: '#987e6a', title: 'road' },
+
+    { color: '#759edf', title: 'sky-other' },
+
+    { color: '#352613', title: 'tree' },
+
+    { color: '#636363', title: 'pavement' },
+
+    { color: '#e670b6', title: 'flower' },
+
+    { color: '#c1c3c9', title: 'fog' },
+
+    { color: '#776c2d', title: 'hill' },
+
+    { color: '#bf602c', title: 'leaves' },
+
+    { color: '#32604d', title: 'river' }
+
+  ];
+
+  $('.color-picker').wrap('<div class="color-picker-wrap"></div>');
+
+  $('.color-picker-wrap').each(function () {
+
+    var self = $(this);
+
+    if (self.children('.color-picker').hasClass('cp-sm')) {
+
+      self.addClass('cp-sm');
+
+    } else if (self.children('.color-picker').hasClass('cp-lg')) {
+
+      self.addClass('cp-lg');
+
+    }
+
+    self.append('<ul></ul><input type="color" style="display:none;" />');
+
+    var $foundactive = false;
+
+    for (var i = 0; i < $color_list.length; i++) {
+
+      var $active = '';
+
+      if (self.children(".color-picker").val() == $color_list[i].color) {
+
+        $active = 'class="active"';
+
+        $foundactive = true;
+
+      }
+
+      self.children('ul').append('<li ' + $active + ' style="background-color:' + $color_list[i].color + '" title="' + $color_list[i].title + '"></li>');
+    }
+
+    if (!$foundactive && self.children(".color-picker").val() != '') {
+
+      self.children('ul').append('<li class="active" title="Custom Color ' + self.children(".color-picker").val() + '" style="background-color:' + self.children(".color-picker").val() + '"></li>');
+
+      if (self.children(".color-picker").hasClass('cp-show')) {
+
+        self.children('small').remove();
+
+        self.append('<small>Selected Color: <code>' + self.children(".color-picker").val() + '</code></small>');
+
+      }
+
+    }
+
+    self.children('ul').append('<li class="add_new" title="Add New">+</li>');
+
+  });
+
+  $('.color-picker-wrap ul').on('click', 'li', function () {
+    console.log("1");
+
+    var self = $(this);
+
+    if (!self.hasClass('add_new')) {
+
+      if (!self.hasClass('active')) {
+
+        self.siblings().removeClass('active');
+
+        var color = rgb2hex(self.css("backgroundColor"));
+        console.log(color);
+
+        currentColor = color;
+
+        self.parents('.color-picker-wrap').children(".color-picker").val(color);
+
+        self.addClass('active');
+
+      }
+    } else {
+      self.parents('.color-picker-wrap').children("input[type='color']").trigger('click');
+    }
+
+  });
+
+  $(".color-picker-wrap input[type='color']").on("change", function () {
+
+    var self = $(this);
+
+    self.parents('.color-picker-wrap').children('ul').children('li').removeClass('active');
+
+    self.parents('.color-picker-wrap').children('ul').children('li.add_new').remove();
+
+    self.parents('.color-picker-wrap').children('ul').append('<li class="active" title="Custom Color ' + self.val() + '" style="background-color:' + self.val() + '"></li>');
+
+    self.parents('.color-picker-wrap').children(".color-picker").val(self.val());
+
+    self.parents('.color-picker-wrap').children('ul').append('<li class="add_new" title="Add New">+</li>');
+
+    if (self.parents('.color-picker-wrap').children(".color-picker").hasClass('cp-show')) {
+
+      self.parents('.color-picker-wrap').children('small').remove();
+
+      self.parents('.color-picker-wrap').append('<small>Selected Color: <code>' + self.val() + '</code></small>');
+
+    }
+
+  });
+
+  var hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
+
+  function rgb2hex(rgb) {
+
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }
+
+  function hex(x) {
+
+    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+
+  }
+
+});    
+
+
+
+
+// document.getElementById('colorpicker').addEventListener('change', function () {
+//   console.log("hihihi");
+//   currentColor = this.value;
+// });
+// document.getElementById('bgcolorpicker').addEventListener('change', function () {
+//   ctx.fillStyle = this.value;
+//   ctx.fillRect(0, 0, canvas.width, canvas.height);
+//   redraw();
+//   currentBg = ctx.fillStyle;
+// });
 document.getElementById('controlSize').addEventListener('change', function () {
   currentSize = this.value;
   document.getElementById("showSize").innerHTML = this.value;
