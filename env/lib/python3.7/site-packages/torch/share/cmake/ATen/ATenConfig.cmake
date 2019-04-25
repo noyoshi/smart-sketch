dc6dd608a20a171df2dd8a528cffaee182d1a000
Gabe Magee <glma2016@pomona.edu>
2019-04-24 20:13:44 -0700

    reset env

---
+++ env/lib/python3.7/site-packages/torch/share/cmake/ATen/ATenConfig.cmake
@@ -0,0 +1,9 @@
+# Find the TH includes and library
+#
+# ATEN_INCLUDE_DIR -- where to find the includes
+# ATEN_LIBRARIES -- list of libraries to link against
+# ATEN_FOUND -- set to 1 if found
+
+SET(ATEN_FOUND 1)
+SET(ATEN_INCLUDE_DIR "/Users/soumith/b101_2/2019_02_08/wheel_build_dirs/wheel_3.7/pytorch/torch/lib/tmp_install/include")
+SET(ATEN_LIBRARIES "")
