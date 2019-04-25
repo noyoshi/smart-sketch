ee51111cd3a1d48f497baf28075c2d48711121a8
Noah Yoshida <nyoshida@nd.edu>
2019-04-24 22:20:11 -0700

    fixed conflicts, got rid of env

--- env/lib/python3.7/site-packages/torch/lib/include/torch/csrc/jit/passes/canonicalize.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "torch/csrc/jit/ir.h"
-
-namespace torch { namespace jit {
-
-TORCH_API std::shared_ptr<Graph> Canonicalize(const std::shared_ptr<Graph>& graph);
-
-}}
--- env/lib/python3.7/site-packages/torch/lib/include/torch/csrc/jit/passes/remove_expands.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "torch/csrc/jit/ir.h"
-
-namespace torch { namespace jit {
-
-TORCH_API void RemoveExpands(const std::shared_ptr<Graph>& graph);
-
-}}
