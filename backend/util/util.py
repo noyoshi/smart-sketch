dc0e5f0fab66348099f336a88ececc89a907c400
Noah Yoshida <nyoshida@nd.edu>
2019-05-18 20:07:31 -0700

    got rid of some city scape stuff

--- backend/util/util.py
+++ backend/util/util.py
@@ -35,7 +35,7 @@ def copyconf(default_opt, **kwargs):
 
 
 def tile_images(imgs, picturesPerRow=4):
-    """ Code borrowed from 
+    """ Code borrowed from
     https://stackoverflow.com/questions/26521365/cleanly-tile-numpy-array-of-images-stored-in-a-flattened-1d-format/26521997
     """
 
@@ -45,7 +45,7 @@ def tile_images(imgs, picturesPerRow=4):
     else:
         rowPadding = picturesPerRow - imgs.shape[0] % picturesPerRow
     if rowPadding > 0:
-        imgs = np.concatenate([imgs, np.zeros((rowPadding, *imgs.shape[1:]), dtype=imgs.dtype)], axis=0)    
+        imgs = np.concatenate([imgs, np.zeros((rowPadding, *imgs.shape[1:]), dtype=imgs.dtype)], axis=0)
 
     # Tiling Loop (The conditionals are not necessary anymore)
     tiled = []
@@ -64,7 +64,7 @@ def tensor2im(image_tensor, imtype=np.uint8, normalize=True, tile=False):
         for i in range(len(image_tensor)):
             image_numpy.append(tensor2im(image_tensor[i], imtype, normalize))
         return image_numpy
-    
+
     if image_tensor.dim() == 4:
         # transform each image in the batch
         images_np = []
@@ -85,9 +85,9 @@ def tensor2im(image_tensor, imtype=np.uint8, normalize=True, tile=False):
     if normalize:
         image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
     else:
-        image_numpy = np.transpose(image_numpy, (1, 2, 0)) * 255.0      
+        image_numpy = np.transpose(image_numpy, (1, 2, 0)) * 255.0
     image_numpy = np.clip(image_numpy, 0, 255)
-    if image_numpy.shape[2] == 1:        
+    if image_numpy.shape[2] == 1:
         image_numpy = image_numpy[:,:,0]
     return image_numpy.astype(imtype)
 
@@ -107,12 +107,12 @@ def tensor2label(label_tensor, n_label, imtype=np.uint8, tile=False):
         else:
             images_np = images_np[0]
             return images_np
-    
+
     if label_tensor.dim() == 1:
         return np.zeros((64, 64, 3), dtype=np.uint8)
     if n_label == 0:
         return tensor2im(label_tensor, imtype)
-    label_tensor = label_tensor.cpu().float()    
+    label_tensor = label_tensor.cpu().float()
     if label_tensor.size()[0] > 1:
         label_tensor = label_tensor.max(0, keepdim=True)[1]
     label_tensor = Colorize(n_label)(label_tensor)
@@ -142,7 +142,7 @@ def mkdirs(paths):
 def mkdir(path):
     if not os.path.exists(path):
         os.makedirs(path)
-        
+
 def atoi(text):
     return int(text) if text.isdigit() else text
 
@@ -194,70 +194,3 @@ def load_network(net, label, epoch, opt):
     net.load_state_dict(weights)
     return net
 
-
-###############################################################################
-# Code from
-# https://github.com/ycszen/pytorch-seg/blob/master/transform.py
-# Modified so it complies with the Citscape label map colors
-###############################################################################
-def uint82bin(n, count=8):
-    """returns the binary of integer n, count refers to amount of bits"""
-    return ''.join([str((n >> y) & 1) for y in range(count-1, -1, -1)])
-
-def labelcolormap(N):
-    if N == 35: # cityscape
-        cmap = np.array([(  0,  0,  0), (  0,  0,  0), (  0,  0,  0), (  0,  0,  0), (  0,  0,  0), (111, 74,  0), ( 81,  0, 81),
-                     (128, 64,128), (244, 35,232), (250,170,160), (230,150,140), ( 70, 70, 70), (102,102,156), (190,153,153),
-                     (180,165,180), (150,100,100), (150,120, 90), (153,153,153), (153,153,153), (250,170, 30), (220,220,  0),
-                     (107,142, 35), (152,251,152), ( 70,130,180), (220, 20, 60), (255,  0,  0), (  0,  0,142), (  0,  0, 70),
-                     (  0, 60,100), (  0,  0, 90), (  0,  0,110), (  0, 80,100), (  0,  0,230), (119, 11, 32), (  0,  0,142)], 
-                    dtype=np.uint8)
-    else:
-        cmap = np.zeros((N, 3), dtype=np.uint8)
-        for i in range(N):
-            r, g, b = 0, 0, 0
-            id = i + 1 # let's give 0 a color
-            for j in range(7):
-                str_id = uint82bin(id)
-                r = r ^ (np.uint8(str_id[-1]) << (7-j))
-                g = g ^ (np.uint8(str_id[-2]) << (7-j))
-                b = b ^ (np.uint8(str_id[-3]) << (7-j))
-                id = id >> 3
-            cmap[i, 0] = r
-            cmap[i, 1] = g
-            cmap[i, 2] = b
-
-        if N == 182: # COCO        
-            important_colors = {
-                'sea': (54, 62, 167),
-                'sky-other': (95, 219, 255),
-                'tree': (140, 104, 47),
-                'clouds': (170, 170, 170),
-                'grass': (29, 195, 49)
-            }
-            for i in range(N):
-                name = util.coco.id2label(i)
-                if name in important_colors:
-                    color = important_colors[name]
-                    cmap[i] = np.array(list(color))
-                                                     
-    return cmap
-
-class Colorize(object):
-    def __init__(self, n=35):
-        self.cmap = labelcolormap(n)
-        self.cmap = torch.from_numpy(self.cmap[:n])
-
-    def __call__(self, gray_image):
-        size = gray_image.size()
-        color_image = torch.ByteTensor(3, size[1], size[2]).fill_(0)
-
-        for label in range(0, len(self.cmap)):
-            mask = (label == gray_image[0]).cpu()
-            color_image[0][mask] = self.cmap[label][0]
-            color_image[1][mask] = self.cmap[label][1]
-            color_image[2][mask] = self.cmap[label][2]
-
-        return color_image
-
-    
